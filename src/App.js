7cbb22ddfe1596e03b317fdfa395ca71a6b31f03
joeblk74 <joeblk@gmail.com>
2021-10-12 12:14:04 -0800

    Updated SongTable.jsx with new connection to backend db and updated to App.js with display of table

--- src/App.js
+++ src/App.js
@@ -1,56 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import SearchBar from './SearchBar/Searchbar';
 // import SongForm from './components/SongForm/SongForm';
-import SongTable from './components/SongTable/SongTable';
-import axios from 'axios';
+import SongTable from "./components/SongTable/SongTable";
+import axios from "axios";
 
 class App extends Component {
-
   constructor(props) {
-    super(props);  
+    super(props);
     this.state = {
-      songs: []
+      songs: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getAllSongs();
+  }
+
+  async getAllSongs() {
+    console.log("Inside Get ALL Songs")
+    try {
+      let response = await axios.get("http://127.0.0.1:8000/music/");
+      console.log("Response Date: ", response.data);
+      this.setState({
+        songs: response.data,
+      });
+    } 
+    catch (err) {
+      console.log("Error in axios get: ", err)
     }
   }
 
-componentDidMount(){
-  this.getAllSongs();
-}
-
-async getAllSongs() {
-  let response = await axios.get('http://127.0.0.1:8000/music');
-  this.setState({
-songs: response.data
-  })
-}
-  render() { 
-    console.log(this.state.songs)
+  render() {
     return (
       <div>
         <h1>Music Library</h1>
         <hr />
-        <SongTable songs={this.state.songs} />
-        <table>
-          <thead>
-            <tr>
-              <th>Song Title</th>
-              <th>Artist</th>
-              <th>Album</th>
-              <th>Genre</th>
-              <th>Release Date</th>
-            </tr>
-          </thead>
-          <tbody>
-
-          </tbody>
-
-        </table>
         <h1>Search Music Library</h1>
+        <SongTable songs={this.state.songs} />
         <hr />
         {/* <SearchBar /> */}
       </div>
-     );
+    );
   }
 }
- 
-export default App ;
+
+export default App;
--- src/components/SongTable/SongTable.jsx
+++ src/components/SongTable/SongTable.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
 const SongTable = (props) => {
-    return ( 
-        <div>
-            {props.songs.map(function(songs){
-                return <h2>{songs}</h2>
-        })}
-        </div>
-     );
-}
- 
+  return (
+    <Fragment>
+      <table>
+        <thead>
+          <tr>
+            <th>Song Title</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Genre</th>
+            <th>Release Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {console.log("Props: ", props.songs)}
+          {props.songs.map(function (song) {
+            return (
+              <tr key={song.id}>
+                <td>{song.title}</td>
+                <td>{song.artist}</td>
+                <td>{song.album}</td>
+                <td>{song.genre}</td>
+                <td>{song.release_date}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
 export default SongTable;
